bb9b24fbb3e17c199d067d3a686a4f07e98319e6
felipe <december1294@gmail.com>
2021-03-09 17:11:24 -0500

    Complete problem 04

--- constructor-functions-and-classes-project-skeleton/problems/04-class-instance-method.js
+++ constructor-functions-and-classes-project-skeleton/problems/04-class-instance-method.js
@@ -23,7 +23,7 @@ Node.js with the examples below. Use the command:
 
 Examples:
 
-const message1 = new Email('sally@smith.com', 'john@smith.com', 
+const message1 = new Email('sally@smith.com', 'john@smith.com',
   'Test Message One', 'This is a test message.');
 console.log(message1);
 console.log(message1.getSubjectAndText());
@@ -38,7 +38,7 @@ console.log(message1.getSubjectAndText());
 // }
 // Test Message One: This is a test message.
 
-const message2 = new Email('sally@smith.com', 'john@smith.com', 
+const message2 = new Email('sally@smith.com', 'john@smith.com',
   'Test Message Two', 'This is a test message.');
 console.log(message2);
 console.log(message2.getSubjectAndText());
@@ -54,9 +54,26 @@ console.log(message2.getSubjectAndText());
 // Test Message Two: This is a test message.
 
 ***********************************************************************/
+class Email {
+  constructor(recipient, sender, subject, text){
+    this.recipient = recipient;
+    this.sender = sender;
+    this.subject = subject;
+    this.text = text;
+  }
+  getSubjectAndText(){
+    return `${this.subject}: ${this.text}`;
+  }
+}
+const message1 = new Email('sally@smith.com', 'john@smith.com',
+  'Test Message One', 'This is a test message.');
+console.log(message1);
+console.log(message1.getSubjectAndText());
 
-
-
+const message2 = new Email('sally@smith.com', 'john@smith.com',
+  'Test Message Two', 'This is a test message.');
+console.log(message2);
+console.log(message2.getSubjectAndText());
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = Email;
